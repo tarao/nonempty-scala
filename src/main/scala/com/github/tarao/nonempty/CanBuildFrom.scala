f59f0374d7f550dfd72b6dc609cb9538ada5af37
tarao <tarao.gnn@gmail.com>
2016-09-17 15:16:52 +0900

    Make `CanBuildFrom` an `AnyVal`.

--- src/main/scala/com/github/tarao/nonempty/CanBuildFrom.scala
+++ src/main/scala/com/github/tarao/nonempty/CanBuildFrom.scala
@@ -13,13 +13,13 @@ import scala.collection.mutable.Builder
   * @tparam To the type of the collection to be created.
   */
 @implicitNotFound(msg = "Cannot construct a collection of type ${To} with elements of type ${B} based on a collection of type ${A}.")
-sealed trait CanBuildFrom[-A, B, +To] {
+sealed trait CanBuildFrom[-A, B, +To] extends Any {
   private[nonempty] def canBuildFrom: CollCanBuildFrom[Iterable[A], B, To]
 }
 object CanBuildFrom {
   private[nonempty] case class NonEmptyCanBuildFrom[A, B](
-    private val bf: CollCanBuildFrom[Iterable[A], B, Iterable[B]]
-  ) extends CanBuildFrom[A, B, NonEmpty[B]] {
+    val bf: CollCanBuildFrom[Iterable[A], B, Iterable[B]]
+  ) extends AnyVal with CanBuildFrom[A, B, NonEmpty[B]] {
     private type From = Iterable[A]
     private type To = NonEmpty[B]
     private[nonempty] def canBuildFrom: CollCanBuildFrom[From, B, To] =
@@ -31,8 +31,11 @@ object CanBuildFrom {
       }
   }
   private[nonempty] case class OtherCanBuildFrom[A, B, To](
-    private[nonempty] val canBuildFrom: CollCanBuildFrom[Iterable[A], B, To]
-  ) extends CanBuildFrom[A, B, To]
+    val bf: CollCanBuildFrom[Iterable[A], B, To]
+  ) extends AnyVal with CanBuildFrom[A, B, To] {
+    private type From = Iterable[A]
+    private[nonempty] def canBuildFrom: CollCanBuildFrom[From, B, To] = bf
+  }
 
   implicit def nonEmptyCanBuildFrom[A, B](implicit
     bf: CollCanBuildFrom[Iterable[A], B, Iterable[B]]
