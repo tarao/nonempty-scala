edd265a063b949620d5c2d90bda85c710cb6a881
tarao <tarao.gnn@gmail.com>
2016-09-21 18:34:23 +0900

    Fix wrong type variable description.

--- src/main/scala/com/github/tarao/nonempty/package.scala
+++ src/main/scala/com/github/tarao/nonempty/package.scala
@@ -3,12 +3,12 @@ package com.github.tarao
 import scala.collection.generic.{CanBuildFrom => CollCanBuildFrom}
 
 package object nonempty {
-  /** Provides a CanBuildFrom instance that builds a specific target
-    * collection (`To') irrespective of the original collection
-    * (`From').
+  /** Provides a `CanBuildFrom` instance that builds a specific target
+    * collection (`To`) irrespective of the original collection
+    * (`NonEmpty[A]`).
     */
-  def breakOut[From, T, To](implicit
-    bf: CollCanBuildFrom[Nothing, T, To]
-  ): CanBuildFrom[From, T, To] =
+  def breakOut[A, B, To](implicit
+    bf: CollCanBuildFrom[Nothing, B, To]
+  ): CanBuildFrom[A, B, To] =
     CanBuildFrom.OtherCanBuildFrom(scala.collection.breakOut(bf))
 }
